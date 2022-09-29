467040e4ef272ac5e91f7e39bc46b6305f4f4928
geek-Shayan <sadi06888@gmail.com>
2022-09-29 18:14:10 +0600

    last commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,67 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+// //STORE --> GLOBALIZED STATE
+
+
+// //ACTION --> Describes what to do ie. Increment
+// const increment = () => {
+//   return {
+//     type: "INCREMENT"
+//   }
+// } 
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT"
+//   }
+// } 
+
+
+// //REDUCER --> Describes HOW to do Action and modify Store ie. do Increment
+// const counter = (state = 0, action)  => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+      
+//     case "DECREMENT":
+//       return state - 1;
+
+//   }
+// }
+
+// let store = createStore(counter);
+
+
+// //DISPLAY
+// store.subscribe(() => console.log(store.getState()));
+// // console.log(store.getState());
+
+
+// //DISPATCH --> Sends Actions to Reducer
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
